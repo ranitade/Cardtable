36d64815c456e8840b14aadee548052dc865423a
ranitade <45083210+ranitade@users.noreply.github.com>
2018-11-24 14:35:34 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div v-for="card in cardtable"><card-table v-bind="table"></card-table></div>
+</template>
+<script>
+import CardTable from "./components/cardtable";
+
+export default {
+  name: "App",
+  components: {
+    CardTable
+  }
+  data() {
+    return {
+      cards: [
+        {
+          title: "CN Tower",
+          subtitle: "Best place ever",
+          body: "The food is pretty bad but the view is great",
+          imgSrc:
+            "https://www.torontobusco.com/wp-content/uploads/2017/05/n-dd-600x400.jpg"
+        },
+        {
+          title: "Niagara Falls CA",
+          subtitle: "Second best",
+          body: "The food is horrible but the view is decent",
+          imgSrc:
+            "https://media-cdn.tripadvisor.com/media/photo-s/0f/73/db/0a/niagara-falls-boat-tour.jpg"
+        },
+        {
+          title: "Niagara Falls US",
+          subtitle: "Second best",
+          body: "The food is horrible but the view is decent",
+          imgSrc:
+            "https://media-cdn.tripadvisor.com/media/photo-s/0f/73/db/0a/niagara-falls-boat-tour.jpg"
+        }
+      ]
+    };
+  }
+};
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/cardtable.vue
@@ -0,0 +1,27 @@
+<template>
+  <table class="table">
+    <thead class="card-header">
+      <tr>
+        <th><h2>title</h2></th>
+        <th><h2>Subtitle</h2></th>
+        <th><h2>Body</h2></th>
+        <th><h2>Image</h2></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="active">
+        <td class="card-title h5">{{ title }}</td>
+        <td class="card-subtitle">{{ subtitle }}</td>
+        <td class="card-body">{{ body }}</td>
+        <td class="card-imgSrc">{{ imgSrc }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "card-table",
+  props: ["title", "subtitle", "body", "imgSrc"]
+};
+</script>
